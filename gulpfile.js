e6046a89d4c1b4b29fab3f9a7c5bc010f669423c
BalabaevRV <ice187boy@gmail.com>
2021-09-18 00:22:33 +0300

    Добавил gulpfile и зависимости

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const { src, dest, parallel, series, watch } = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const sourcemaps = require("gulp-sourcemaps");
+const browserSync = require('browser-sync').create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+
+const styles = () => {
+    return src ("dev/sass/*.sass")
+        .pipe(plumber())
+        .pipe(sourcemaps.init("."))
+        .pipe(sass())
+        .pipe (postcss([autoprefixer]))
+        // .pipe(csso())
+        // .pipe(rename("style.min.css"))
+        .pipe(sourcemaps.write("."))
+        .pipe(dest("dev/css/"))
+}
+
+const server = () => {
+    browserSync.init({
+        server: { baseDir: 'dev/'},
+        notify: false,
+        online: true,
+        ui: false
+    })
+}
+
+const watcher = () => {
+    watch("dev/sass/*.sass", series("styles"));
+    watch("dev/*.html").on("change", browserSync.reload);
+}
+
+exports.server = server;
+exports.styles = styles;
+exports.start = parallel (styles, server, watcher);
